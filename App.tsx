d939a926acaab185d329a16df3aa2c4984f94594
Zakaria aourzag <z.aourzag@gmail.com>
2022-07-08 17:53:31 +0200

    sheisse

--- App.tsx
+++ App.tsx
@@ -3,7 +3,14 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { AppNavigator } from './navigation/index'
+import * as Sentry from "@sentry/react-native";
 
+Sentry.init({
+  dsn: "https://3ac43dfc5bb7435db4b0815aacbed0e7@o1296783.ingest.sentry.io/6558455",
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+});
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack}/>
